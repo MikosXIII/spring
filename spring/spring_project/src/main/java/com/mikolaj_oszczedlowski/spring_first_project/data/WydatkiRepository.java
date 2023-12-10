9dbaf8bb7fd9921b605aa759450a0bae28cb890d
MikosXIII <100759464+MikosXIII@users.noreply.github.com>
2023-12-10 22:02:04 +0100

    Delete spring directory

--- spring/spring_project/src/main/java/com/mikolaj_oszczedlowski/spring_first_project/data/WydatkiRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mikolaj_oszczedlowski.spring_first_project.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mikolaj_oszczedlowski.spring_first_project.models.Wydatek;
-
-public interface WydatkiRepository extends JpaRepository<Wydatek, Long>{
-    
-}
